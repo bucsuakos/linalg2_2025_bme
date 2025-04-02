24f87733f4a218b8637c4a4766d0930af77ec1d3
bucsuakos <bucsuakos03@gmail.com>
2025-04-02 19:35:02 +0200

    Even more cleanup + fresh pdfs.

---
+++ pdfek/pdf_refresher.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+cd ~/Documents/git/linalg2/linalg2_1/
+pdflatex ~/Documents/git/linalg2/linalg2_1/linalg2_1.tex
+pdflatex ~/Documents/git/linalg2/linalg2_1/linalg2_1.tex
+
+cd ~/Documents/git/linalg2/linalg2_1/
+pdflatex ~/Documents/git/linalg2/linalg2_1/linalg2_1_deff.tex
+pdflatex ~/Documents/git/linalg2/linalg2_1/linalg2_1_deff.tex
+
+cd ~/Documents/git/linalg2/linalg2_osszes/
+pdflatex ~/Documents/git/linalg2/linalg2_osszes/linalg2_osszes_deff.tex
+pdflatex ~/Documents/git/linalg2/linalg2_osszes/linalg2_osszes_deff.tex
+
+cd ~/Documents/git/linalg2/linalg2_osszes/
+pdflatex ~/Documents/git/linalg2/linalg2_osszes/linalg2_osszes.tex
+pdflatex ~/Documents/git/linalg2/linalg2_osszes/linalg2_osszes.tex
+
+cp ~/Documents/git/linalg2/linalg2_1/linalg2_1.pdf ~/Documents/git/linalg2/pdfek/osszes1.pdf
+cp ~/Documents/git/linalg2/linalg2_1/linalg2_1_deff.pdf ~/Documents/git/linalg2/pdfek/deff1.pdf
+cp ~/Documents/git/linalg2/linalg2_osszes/linalg2_osszes_deff.pdf ~/Documents/git/linalg2/pdfek/deff.pdf
+cp ~/Documents/git/linalg2/linalg2_osszes/linalg2_osszes.pdf ~/Documents/git/linalg2/pdfek/osszes.pdf
